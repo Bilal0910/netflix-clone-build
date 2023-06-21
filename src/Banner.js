42cbdc79a8dc0d3e9894415d28105028423c516d
bilal-nadeem <bilalnadeem>
2023-06-21 19:55:02 +0500

    sixth commit

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import axios from './axios';
+import requests from './Requests';
 
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchTopRated);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+
+            return request;
+        }
+
+        fetchData();
+    }, []);
 
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n - 1) + '...' : string
@@ -12,23 +31,20 @@ function Banner() {
      className='banner' 
      style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Netflix_logo.svg/1024px-Netflix_logo.svg.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
     }}
     >
         <div className="banner_contents">
-            <h1 className="banner_title">Movie Name</h1>
+            <h1 className="banner_title">
+                {movie?.title || movie?.name || movie?.original_name}
+            </h1>
             <div className="banner_buttons">
                 <button className='banner_button'>play</button>
                 <button className='banner_button'>My list</button>
             </div>
             <h1 className="banner_description">
-               {truncate
-                (`this is a test descriptoion this is a test descriptoion this is a test descriptoion
-                this is a test descriptoion this is a test descriptoion this is a test descriptoion
-                this is a test descriptoion this is a test descriptoion this is a test descriptoion
-                this is a test descriptoion this is a test descriptoion this is a test descriptoion
-                this is a test descriptoion this is a test descriptoion`, 160)}
+               {truncate(movie?.overview , 160)}
             </h1>
         </div>
 
--- src/Requests.js
+++ src/Requests.js
@@ -1,6 +1,6 @@
 // always hide the API key in {process.env.API_KEY}
 
-const API_KEY = process.env.API_KEY
+const API_KEY = "8f5ab4978bb8d4960ef79e44382a2e2c"
 
 const requests = {
 
@@ -8,10 +8,10 @@ const requests = {
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
     
 }
 
